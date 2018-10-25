bf41b9a407a90cbc0d9cb9fbcb3478f8596241b3
Ashish Patel <ashish007only@gmail.com>
2018-10-25 19:37:04 +0530

    Initial commit

---
+++ app/src/main/java/sadyogi/com/github_push/MainActivity.java
@@ -0,0 +1,20 @@
+package sadyogi.com.github_push;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void pushTest(){
+
+        int a = 10;
+        int b = 20;
+        int c = 30;
+    }
+}
